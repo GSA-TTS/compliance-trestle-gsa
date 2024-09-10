7c5728b90157a9d1db8d2e685ec7fda80094f3df
Ryan Ahearn <ryan.ahearn@gsa.gov>
2024-09-10 09:55:53 -0400

    Tweaks to list deduplication code

--- src/trestle_gsa/core/merge.py
+++ src/trestle_gsa/core/merge.py
@@ -29,17 +29,21 @@ def _deduplicated_list(a: list, b: list) -> list:
     if len(b) == 0:
         return a
     combined_list = b + a
-    if 'id' in a[0]:
+    if 'id' in a[0] and a[0]['id']:
         # deduplicate by id
+        trace.log('Deduplicating by "id"')
         return list({x['id']: x for x in combined_list}.values())
-    elif 'uuid' in a[0]:
-        # deduplicate by uuid
-        return list({x['uuid']: x for x in combined_list}.values())
+    if 'role_id' in a[0] and a[0]['role_id']:
+        # deduplicate by role-id
+        trace.log('Deduplicating by "role_id"')
+        return list({x['role_id']: x for x in combined_list}.values())
     else:
         try:
             # deduplicate by all fields via json string
+            trace.log('Deduplicating by json dump')
             dedupped = set(json.dumps(item, sort_keys=True) for item in combined_list)
             return list(json.loads(x) for x in dedupped)
         except TypeError:
             # error dumping to json, just combine the lists
+            trace.log('No deduplication, returning combined_list')
             return combined_list
